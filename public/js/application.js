eed1c7c6f4344d7aaeee69f3b021a097ca09ef16
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-13 16:57:33 -0500

    Added CSS and changed layout

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,10 @@
-
-
-
 $(document).ready(function() {
 
 
   $('.edit_click').click(function(event){
 
   	event.preventDefault();
-    var url = $(this).attr('href')
+    var url = $(this).attr('href');
     console.log(url);
     var link = this
 
@@ -16,6 +13,26 @@ $(document).ready(function() {
     	console.log(response);
     	console.log(link);
     	$(link).after(response);
+    });
+  });
+
+});
+
+
+$(document).ready(function(){
+  $('.create_click').click(function(event){
+    event.preventDefault();
+    console.log('click');
+
+    var url = $(this).attr('href');
+    console.log(url)
+    var link = this
+
+    $.get(url, function(response) {
+
+      // console.log(response);
+      console.log(link);
+      $(link).after(response);
     });
   });
 
